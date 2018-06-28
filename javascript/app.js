2cfb41d0627c14af99b27b34688576b8d4bb3c40
unknown <smarcus4@gmu.edu>
2018-06-28 17:56:43 -0400

    added ui and js/jq

---
+++ javascript/app.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  var config = {
+      apiKey: "AIzaSyBA3CuckV5_T_x9OGbRlKONilv3wfeQYA4",
+      authDomain: "train-activity-a6a19.firebaseapp.com",
+      databaseURL: "https://train-activity-a6a19.firebaseio.com",
+      projectId: "train-activity-a6a19",
+      storageBucket: "train-activity-a6a19.appspot.com",
+      messagingSenderId: "635425682675"
+    };
+    firebase.initializeApp(config);
+
+
+    var database = firebase.databse();
+
+    $("#addTrain").on("click", function(event) {
+      event.preventDefault();
+
+      var trainName = $("#trainName").val().trim();
+      var destination = $("#destination").val().trim();
+      var firstTrain = $("#firstTrain").val().trim();
+      var freq = $("$interval").val().trim();
+
+    
+    databse.ref().push({
+      trainName: trainName,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: freq
+
+    });
+  });
+
+
+  database.ref().on("child_added", function (childSnapshot) {
+
+    var newTrain = childSnapshot.val().trainName;
+    var newLocation = childSnapshot.val().destination;
+    var newFirstTrain = childSnapshot.val().firstTrain;
+    var newFreq = childSnapshot.val().frequency;
+
+    var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
+
+    var currentTime = moment();
+
+    var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+
+    var tRemainder = diffTime % newFreq;
+
+
+    var tMinutesTillTrain = newFreq - tRemainder;
+
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var catchTrain = moment(nextTrain).format("HH:mm");
+
+   
+    $("#all-display").append(
+      ' <tr><td>' + newTrain +
+      ' </td><td>' + newLocation +
+      ' </td><td>' + newFreq +
+      ' </td><td>' + catchTrain +
+      ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
+
+   
+    $("#trainName, #destination, #firstTrain, #interval").val("");
+    return false;
+  },
+    
+    function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+});
